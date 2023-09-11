e6cec7382ba07e8e7bf64c492100f79680dbc0cd
nordluma <maxnordlund.95@hotmail.com>
2023-09-12 00:11:39 +0300

    feat: simulate delay on server

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
@@ -30,6 +31,7 @@ func main() {
 	}
 
 	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
 		title := r.PostFormValue("title")
 		director := r.PostFormValue("director")
 		htmlStr := fmt.Sprintf(
