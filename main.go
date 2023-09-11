6e4ca0d4027aeaa1b91e6e5ae2807cd131161c84
nordluma <maxnordlund.95@hotmail.com>
2023-09-11 23:21:24 +0300

    feat: serve go template

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
 	fmt.Println("Hello world")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello World\n")
-		io.WriteString(w, r.Method)
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "Lord of the Rings", Director: "Peter Jackson"},
+				{
+					Title:    "Star Wars: Revenge of the Sith",
+					Director: "George Lucas",
+				},
+			},
+		}
+
+		tmpl.Execute(w, films)
 	}
 
 	http.HandleFunc("/", h1)
